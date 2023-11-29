e7afa2b385921120596ece512fca66dff7e0e58a
uemura <m.uemura.sys22@morijyobi.ac.jp>
2023-11-29 14:36:42 +0900

    演習1-4

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -11,20 +11,29 @@ class MyScene extends Phaser.Scene {
     // シーンの事前読み込み処理
     preload() {
          // 画像の読み込み(使用する時の名前, パス)
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('alien1', 'assets/alien1.png');
+        this.load.image('background', 'assets/background.png');
+        this.load.image('taro', 'assets/taro.png');
     }
 
     // シーン初期化処理
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
-        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'sky');
-        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'alien1');
+        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'background');
         this.text = this.add.text(10, 10, 'Scene 1').setFontSize(32).setColor('#ff0');
+        const player1 = this.physics.add.sprite(D_WIDTH/2, D_HEIGHT/2, 'taro');
+        this.player1 = player1
+        this.player1.angle = 0
     }
     
   // 毎フレーム実行される繰り返し処理
     update() {
+        this.player1.angle += 5;
+        this.player1.setVelocityY(-70);
+        this.player1.setVelocityX(70);
+        if (this.player1.x >= 650) 
+        {
+            this.player1.x = D_WIDTH/2;
+            this.player1.y = D_HEIGHT/2;
+        }
     }
-
 }
